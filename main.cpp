8accf091b10a708d46fe8f9384a2529fa6463e62
2mdtln <120330170+2mdtln@users.noreply.github.com>
2024-12-22 13:11:42 +0300

    typing

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <thread>
 
 std::string mes = "Hello, World!";
-int i = 0;
-
+int i;
 int main()
 {
     while(i < mes.length())
     {
-        std::cout << mes[i] << std::endl;
+        std::cout << mes[i] << std::flush;
         i++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+    std::cout << std::endl;
     return 0;
 }
