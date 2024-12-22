f438c3fc31a3e84f32a9d2ad319c7fdc1255385f
2mdtln <120330170+2mdtln@users.noreply.github.com>
2024-12-22 13:02:47 +0300

    while loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+std::string mes = "Hello, World!";
+int i = 0;
+
+int main()
+{
+    while(i < mes.length())
+    {
+        std::cout << mes[i] << std::endl;
+        i++;
+    }
     return 0;
 }
